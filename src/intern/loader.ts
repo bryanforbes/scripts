33abc3717e215736fc9c80ebcbe4948e433b2815
Rory Mulligan <rmulligan@sitepen.com>
2017-12-07 09:52:27 -0500

    Moving tsconfig, adding intern loader

---
+++ src/intern/loader.ts
@@ -0,0 +1,30 @@
+declare const shimAmdDependencies: any;
+declare const intern: any;
+
+intern.registerLoader((options: any) => {
+	return intern.loadScript('node_modules/@dojo/loader/loader.js')
+		.then(() => intern.loadScript('node_modules/@dojo/shim/util/amd.js'))
+		.then(() => {
+			const { packages = [], baseUrl = intern.config.basePath } = options;
+			packages.push({ 'name': 'sinon', 'location': 'node_modules/sinon/pkg', 'main': 'sinon' });
+
+			(<any> require).config(shimAmdDependencies({
+				baseUrl,
+				...options,
+				packages
+			}));
+
+			// load @dojo/shim/main to import the ts helpers
+			return new Promise<void>((resolve) => {
+				(<any> require)(['@dojo/shim/main'], () => {
+					resolve();
+				});
+			});
+		}).then(() => {
+			return (modules: string[]) => {
+				return new Promise<void>((resolve, reject) => {
+					(<any> require)(modules, () => resolve());
+				});
+			};
+		});
+});
